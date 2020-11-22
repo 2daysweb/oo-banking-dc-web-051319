4afadbee1762006426b016213f684c4fe0413345
Sahnun Mohamud <shmohamud@Sahnuns-MBP.fios-router.home>
2020-11-21 19:59:06 -0500

    ruby practice

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,26 @@
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+    
+    def initialize(name)
+        @name = name
+        @status = 'open'
+        @balance = 1000
+    end
 
+    def deposit(amount)
+        @balance = @balance + amount
+    end
+
+    def display_balance
+        puts("Your balance is $#{@balance}.")
+    end
+
+    def valid?
+        @status == 'open' && @balance > 0? true : false
+    end 
+
+    def close_account
+        @status = 'closed'
+    end
 end
